ce15c7862419c0a11ea51c4458ffa3cd815e9c88
Robb Lewis <me@therobb.com>
2013-09-15 16:46:22 +0100

    Added GS Tracking code

--- header.php
+++ header.php
@@ -15,6 +15,24 @@
 		<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/img/favicon.ico">
 		<link rel="apple-touch-icon" href="<?php bloginfo('template_directory'); ?>/img/apple-touch-icon.png"/>
 		<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/style.css" />
+
+		<script type="text/javascript">
+		  var GoSquared = {};
+		  GoSquared.acct = "GSN-081563-R";
+		  (function(w){
+		    function gs(){
+		      w._gstc_lt = +new Date;
+		      var d = document, g = d.createElement("script");
+		      g.type = "text/javascript";
+		      g.src = "//d1l6p2sc9645hc.cloudfront.net/tracker.js";
+		      var s = d.getElementsByTagName("script")[0];
+		      s.parentNode.insertBefore(g, s);
+		    }
+		    w.addEventListener ?
+		      w.addEventListener("load", gs, false) :
+		      w.attachEvent("onload", gs);
+		  })(window);
+		</script>
 		
 		<?php wp_head(); ?>
 	</head>
